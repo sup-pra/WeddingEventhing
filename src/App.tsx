59ba7b8daebbe44d814cf17db9b022183bf60901
supakarn <supakarn@emetworks.com>
2023-01-29 16:16:08 +0700

    test edit app

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Wedding Eventhing</h1>
+      <h3>All แก้ Night Team</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
